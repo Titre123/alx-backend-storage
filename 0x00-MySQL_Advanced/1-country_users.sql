0a6423886e9880b452040ad70836f3ccbe783d7a
Taiwo <taiwotriumphnat@gmail.com>
2023-01-13 02:05:12 +0100

    new

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS users(
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255),
-    country VARCHAR(10) NOT NULL CHECK (country IN('US', 'CO', 'TN'))
+    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
     PRIMARY KEY(ID)
 )
--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
---- selecct grouped data by origin and order by fans
+-- Ranks country origins of bands, ordered by the number of (non-unique) fans.
 SELECT origin, SUM(fans) AS nb_fans
     FROM metal_bands
     GROUP BY origin
