d4687f14ce204b208b7adaec27a27591347d1974
Taiwo <taiwotriumphnat@gmail.com>
2023-01-19 05:17:37 +0100

    redice exercise

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+'''A module for using the Redis NoSQL data storage.
+'''
+
+
+import redis
+import uuid
+import typing
+
+
+def count_calls(method: Callable) -> Callable:
+    '''Tracks the number of calls made to a method in a Cache class.
+    '''
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        '''Invokes the given method after incrementing its call counter.
+        '''
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return invoker
+
+
+class Cache:
+    def __intit__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)
+
+    def store(self, data: typing.Any) -> str:
+        '''
+            Stores a value in a Redis data storage and returns the key.
+        '''
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: typing.Callable = None) -> \
+            typing.Union[str, bytes, int, float]:
+        '''Retrieves a value from a Redis data storage.
+        '''
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self) -> str:
+        '''
+        Retrieves a string value from a Redis data storage.
+        '''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_str(self) -> int:
+        '''
+        Retrieves an integer value from a Redis data storage.'''
+        return self.get(key, lambda x: int(x))
